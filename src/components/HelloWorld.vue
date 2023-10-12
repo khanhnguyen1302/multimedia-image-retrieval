1559f67c84694f0a46b123e10e5a61f07bf826ba
Khanh Nguyễn <khanhnguyen1302.work@gmail.com>
2023-10-12 14:50:57 +0700

    adjust code

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -26,52 +26,14 @@ const handleSubmit = () => {
   console.log(fileInput.value[0]);
   formdata.append("image", fileInput.value[0], "file12321312.png");
 
-  // const requestOptions = {
-  //   method: "POST",
-  //   headers: myHeaders,
-  //   body: formdata,
-  //   redirect: "follow",
-  // };
-
-  // fetch("http://localhost:3000/upload/photo", requestOptions)
-  //   .then((response) => console.log(response))
-  //   .catch((error) => console.log("error", error));
-
-  let config = {
-    method: "post",
-    maxBodyLength: Infinity,
-    url: "http://localhost:3000/upload/photo",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    data: formdata,
-  };
-
   axios
-    .request(config)
-    .then((response) => {
-      console.log(JSON.stringify(response.data));
+    .post("http://localhost:3000/upload/photo", formdata, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
     })
-    .catch((error) => {
-      console.log(error);
+    .then((res) => {
+      console.log(res);
     });
 };
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
