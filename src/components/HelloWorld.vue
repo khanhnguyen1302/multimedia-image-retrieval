05631685d28423235511bb5ae0b286e6e28206b4
Khanh Nguyễn <khanhnguyen1302.work@gmail.com>
2023-10-14 21:43:23 +0700

    adjust

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,10 +1,33 @@
 <template>
   <!--   PHOTO-->
   <form>
-    <input type="file" name="image" accept="image/*" @change="handleChange" />
+    <input
+      label="Choose Image"
+      type="file"
+      name="image"
+      accept="image/*"
+      @change="handleChange"
+    />
     <button type="button" value="Upload Photo" @click="handleSubmit">
-      touch me
+      Upload to get retrieve images
     </button>
+    <div>
+      <img class="preview-img" :src="imgSrc" />
+    </div>
+    <div>Please select the number of images you want to retrieve:</div>
+
+    <select v-model="selected">
+      <option disabled value="">Please select one</option>
+      <option :value="5">5 images</option>
+      <option :value="10">10 images</option>
+      <option :value="15">15 images</option>
+    </select>
+
+    <div class="retrieve">
+      <button type="button" value="Upload Photo" @click="handleSubmit">
+        Get retrieve images
+      </button>
+    </div>
   </form>
 </template>
 
@@ -13,9 +36,13 @@ import { ref } from "vue";
 import axios from "axios";
 
 const fileInput = ref(null);
+const imgSrc = ref("");
+const selected = ref(5);
 
 const handleChange = (event) => {
   fileInput.value = event.target.files;
+
+  imgSrc.value = URL.createObjectURL(event.target.files[0]);
 };
 
 const handleSubmit = () => {
@@ -23,7 +50,6 @@ const handleSubmit = () => {
   myHeaders.append("Content-Type", "application/json");
 
   const formdata = new FormData();
-  console.log(fileInput.value[0]);
   formdata.append("image", fileInput.value[0], "file12321312.png");
 
   axios
@@ -37,3 +63,14 @@ const handleSubmit = () => {
     });
 };
 </script>
+
+<style>
+.preview-img {
+  max-width: 100px;
+  max-height: 100px;
+}
+
+.retrieve {
+  margin-top: 50px;
+}
+</style>
